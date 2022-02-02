ba52d26d54322fe7ed718ba82a48e1afdca80282
Jochem <jochem@cloudclass.be>
2022-02-02 19:53:02 +0100

    Don't reply when everyone is mentioned

--- src/events/message.ts
+++ src/events/message.ts
@@ -5,7 +5,7 @@ import { Discord, On, Client } from "discordx";
 export abstract class MessageEvents {
     @On("messageCreate")
     onMessage([message]: ArgsOf<"message">, client: Client) {
-        if (message.mentions.has(process.env["APP_CLIENT_ID"] ?? "-1")) {
+        if (message.mentions.has(process.env["APP_CLIENT_ID"] ?? "-1") && !message.mentions.everyone) {
             message.reply("How do you do, fellow human? (Use me by using slash `/`)");
         }
 
